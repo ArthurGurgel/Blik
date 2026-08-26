2cdc2c302f302315159d02380a3f2f877225577e
ArthurGurgel <arthurgurgel18@gmail.com>
2026-08-26 19:28:27 -0300

    Atualiza minSdk para API 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.blik"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 37
         versionCode = 2
         versionName = "0.2"
